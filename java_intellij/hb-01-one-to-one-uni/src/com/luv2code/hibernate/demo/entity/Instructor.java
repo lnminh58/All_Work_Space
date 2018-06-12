60650bb164a58648acb2e8dd9a74bceefe6b993b
Ngoc Minh <lnminh58@gmail.com>
2018-06-12 18:02:05 +0700

    desktop update

--- java_intellij/hb-01-one-to-one-uni/src/com/luv2code/hibernate/demo/entity/Instructor.java
+++ java_intellij/hb-01-one-to-one-uni/src/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -24,6 +24,9 @@ public class Instructor {
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
 
+    public Instructor() {
+    }
+
     public Instructor(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
