a8efb36944a8d20b7f6f46b9018dbb2baca4ac36
Ngoc Minh <lnminh58@gmail.com>
2018-07-25 14:56:00 +0700

    laptop Update

---
+++ react_native/TodoList/src/container/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Container, Header, Title, Content, Button, Left, Right, Body, Icon, Text } from 'native-base';
+export default class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <Text>
+            This is Content Section
+          </Text>
+        </Content>
+      </Container>
+    );
+  }
+}
