06393455042d9bd68aae34358dd75a013d9727df
Le Ngoc Minh <lnminh58@gmail.com>
2018-10-22 17:18:01 +0700

    company

--- nodejs/node-todo-api/server/server.js
+++ nodejs/node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -19,6 +20,29 @@ app.post('/todos', (req, res) => {
     .catch(err => res.status(400).send(err));
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find()
+    .then(todos => {
+      res.send(todos);
+    })
+    .catch(err => res.status(400).send(err));
+});
+
+app.get('/todos/:id', (req, res) => {
+  const { id } = req.params;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      todo ? res.send(todo) : res.status(404).send();
+    })
+    .catch(e => res.status(400).send(e));
+});
+
 app.listen(3000, () => {
   console.log('started on port 3000');
 });
+
+module.exports = { app };
