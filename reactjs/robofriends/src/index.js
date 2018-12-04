77fd69c09208570280fd13e90966b3862a267b0c
Le Ngoc Minh <lnminh58@gmail.com>
2018-12-04 14:00:35 +0700

    company update

--- reactjs/robofriends/src/index.js
+++ reactjs/robofriends/src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Hello from './Hello';
+
+ReactDOM.render(<Hello greeting={'Hello' + ' React Ninja'}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
