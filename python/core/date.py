1ebf298541733b08c8c753eee09d11a116e8f28a
Minh Le <minhle@Minhs-iMac.local>
2019-11-12 21:22:51 +0700

    python core

---
+++ python/core/date.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+x = datetime
+
+print(x.now())
+
+date = datetime(2019, 11, 15)
+
+print(date)
---
+++ python/core/interators.py
@@ -0,0 +1,12 @@
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+mystr = "banana"
+myit = iter(mystr)
+
+for i in range(0, len(mystr)):
+  print(next(myit))
